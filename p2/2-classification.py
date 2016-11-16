61048897ea1e4a88741960b7f9085c1e45cca922
Michael Weisz <mail@michael-weisz.de>
2016-11-16 05:38:52 +0000

    Train and test on Iris dataset.

--- p2/2-classification.py
+++ p2/2-classification.py
@@ -1,17 +1,38 @@
 from naive_bayes import NBC
 import numpy as np
 
-X = np.array([[1, 50],
-              [0, 170],
-              [0, 250],
-              [1, 150],
-              [1, 85],
-              [0, 60]])
+from sklearn.datasets import load_iris
+iris = load_iris()
+X, y = iris['data'], iris['target']
 
-Y = np.array([10, 88, 88, 88, 10, 10])
+N, __ = X.shape
+N_train = int(N * 0.8)
+N_test = N - N_train
 
-X_new = np.array([1, 100])
+X_train = X[:N_train]
+y_train = y[:N_train]
 
-clf = NBC(feature_types=['b', 'r'], num_classes=2)
-clf.fit(X, Y)
-clf.predict(X_new)
+X_test = X[N_train:]
+y_test = y[N_train:]
+
+
+print "{} -> {}".format(X_train[0, :], y_train[0])
+
+nbc = NBC(feature_types=['r', 'r', 'r', 'r'], num_classes=3)
+nbc.fit(X_train, y_train)
+
+correct = 0
+for i, x in enumerate(X_test):
+    yhat = nbc.predict(x)
+    print "Predicted class {}".format(yhat)
+    print "Actual class {}\n\n".format(y_test[i])
+    if yhat == y_test[i]:
+        correct += 1
+
+accuracy = float(correct) / y_test.size
+print(accuracy)
+
+# yhat = nbc.predict(X_test)
+# test_accuracy = np.mean(yhat == y_test)
+# print(test_accuracy)
+# print "Actual class {}".format(y_test[0])
